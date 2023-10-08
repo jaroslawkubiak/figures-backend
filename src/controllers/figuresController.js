7226e68a08d2c72cc9945f322d9ef50dc68eb1ce
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-10-08 22:01:10 +0200

    saving figure img to server

--- src/controllers/figuresController.js
+++ src/controllers/figuresController.js
@@ -1,6 +1,7 @@
 const pool = require('../config/database');
 const OAuth = require('oauth');
 const dotenv = require('dotenv');
+const saveImage = require('../utils/saveImage');
 dotenv.config();
 
 exports.getAllFigures = async (req, res) => {
@@ -30,9 +31,6 @@ exports.addFigure = async (req, res) => {
     bricklinkPrice,
   } = req.body;
 
-  //changing on to 1
-  label = req.body.label === 'on' ? 1 : 0;
-
   //extracting purchase month and year from data
   const tempDate = purchaseDate.split('-');
   const purchaseMonth = Number(tempDate[1]);
@@ -58,6 +56,12 @@ exports.addFigure = async (req, res) => {
     ]
   );
 
+  const remote_url = `https://img.bricklink.com/ItemImage/MN/0/${number}.png`;
+  const local_path = `${__dirname}/../../../${number}.png`;
+  // const local_path = `./images/${number}.png`;
+
+  await saveImage(remote_url, local_path);
+
   res.status(201).send('Figure has been added to DB');
 };
 
@@ -76,7 +80,8 @@ exports.getFigureInfo = async (req, res) => {
     'HMAC-SHA1'
   );
 
-  let fetchForFigureInfo = function (url) {
+  //function for creating Promise to fetch for data
+  const fetchForFigureInfo = function (url) {
     return new Promise((resolve, reject) => {
       oauth.get(url, process.env.BL_TokenValue, process.env.BL_TokenSecret, function (error, data, response) {
         if (error) reject(error);
@@ -88,7 +93,7 @@ exports.getFigureInfo = async (req, res) => {
     });
   };
 
-  let promises = [];
+  const promises = [];
   // fetch for general info about figure
   promises.push(fetchForFigureInfo(`${process.env.BL_BASE_URL}/items/MINIFIG/${searchingNumber}`));
   // fetch for pricing info
@@ -99,7 +104,7 @@ exports.getFigureInfo = async (req, res) => {
       //setting up info
       figureInfo.info = result[0];
       //remove price details array
-      // result[1].price_detail = '';
+      result[1].price_detail = '';
       figureInfo.price = result[1];
       res.send(figureInfo);
     })
---
+++ src/utils/saveImage.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const fs = require('fs');
+
+//saving image to server
+async function saveImage(url, filepath) {
+  const response = await axios({
+    url,
+    method: 'GET',
+    responseType: 'stream',
+  });
+  return new Promise((resolve, reject) => {
+    response.data
+      .pipe(fs.createWriteStream(filepath))
+      .on('error', reject)
+      .once('close', () => resolve(filepath));
+  });
+}
+
+module.exports = saveImage;
